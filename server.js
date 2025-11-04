bd5dc2e8db15f6ff1c8afc03cf872e0c49205320
davidMuñoz <munozd50@gmail.com>
2025-11-04 09:01:02 -0500

    COMENTARIO DE FOTENV

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const { MongoClient } = require("mongodb");
-require("dotenv").config();
+/* require("dotenv").config(); */
 
 const app = express();
 const PORT = process.env.PORT || 3000;
